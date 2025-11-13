6ecfeae302ebd89109b3a0a7b808d66a58c8c8aa
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2025-11-13 13:25:48 -0800

    make listing work

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 pub mod serde_date_time;
 
+use clap::{Parser, Subcommand};
 use std::fs;
 use std::error::Error;
 use serde::{Deserialize, Serialize};
 use crate::serde_date_time::SerdeDateTime;
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    List {
+        #[arg(short, long)]
+        group: Option<String>
+    },
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Todo {
@@ -24,10 +40,25 @@ struct Todo {
     prev_recur_todo_uuid: String,
 }
 
+fn list(todos: &Vec<Todo>, _: Option<String>) -> () {
+    for i in todos.iter() {
+        if !i.archived {
+            println!("{} {} {}", i.id, i.status, i.subject);
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let todos_raw = fs::read_to_string("/home/tortus/.todos.json")?;
+    let args = Args::parse();
+
+    let todos_raw = fs::read_to_string("/home/tortus/.todos.json")?; // find some way to get the
+                                                                     // home directory?
     let r: Vec<Todo> = serde_json::from_str(&todos_raw)?;
-    println!("{:?}", r);
+    // println!("{:?}", r);
+    match args.command {
+        Command::List { group: a } => list(&r, a),
+    }
+
     let redone = serde_json::to_string(&r)?;
     fs::write("output.json", redone)?;
     Ok(())
